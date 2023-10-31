8721d951621ce14c4359ee70921f00cd04fba012
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-10-31 17:49:37 -0400

    Edit login

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,17 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//import functions?
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+//querySelectors:
+const loginButton = document.querySelector('.loginButton')
+
+//addEventListeners:
+
+loginButton.addEventListener("click",function(event) {
+    event.preventDefault() //button under form always have to event.preventDefault
+
+});
